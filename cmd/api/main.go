9df47bfd17a4d63d0ca6e03e246bddbdd173de2a
1234 <a1234@MacBook-Air-1234.local>
2026-05-28 14:41:31 +0300

    Create + get + get all users from DB

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -3,53 +3,78 @@ package main
 import (
 	"fmt"
 	"permission-service/db"
-	"permission-service/internal/auth"
 	"permission-service/internal/repository"
 )
 
 func main() {
-	repo := repository.UserRepository{
-		Users: map[string]auth.Permission{
-			"keker666": 3,
-			"vasya":    7,
-			"poleno":   7,
-		},
+	db, err := db.DatabaseConnect("localhost", "5432", "postgres", "12345", "postgres")
+
+	if err != nil {
+		fmt.Println("main.go says that there`s some err with db conn: ", err)
 	}
 
-	db, err := db.DatabaseConnect(
-		"localhost",
-		"5432",
-		"postgres",
-		"12345",
-		"rbac_test")
+	userType := repository.User{}
 
-	/////////
+	// //CREATE  !!!!!!!!!!!!!!!!!!!!
+	// newId := uuid.New()
+	// userType.CreateUser(db, newId, "test", "12345", 6)
 
-	/*roles*/
-	kekerPerms := repo.GetUser("keker666")
-	fmt.Println(kekerPerms)
-	//добавили рута
-	kekerPerms = kekerPerms.Add(auth.Root)
-	fmt.Println(kekerPerms)
+	//GET ONE  !!!!!!!!!!!!!!!!!!!!
+	// id := uuid.MustParse("2f948611-e89e-44a4-bf1c-b376c837f078")
+	// poleno, err := userType.GetUser(db, id)
 
-	//
-	repo.UpdateUser("keker666", kekerPerms)
+	// if err != nil {
+	// 	fmt.Println(err)
+	// }
+	// fmt.Println(poleno)
 
-	//убрали рута
-	kekerPerms = kekerPerms.Remove(auth.Root)
+	//GET ALL  !!!!!!!!!!!!!!!!!!!!
 
-	//////
+	users, err := userType.GetAllUsers(db)
+	if err != nil {
+		fmt.Println("Problem to get all users: ", err)
+	}
+	fmt.Println(users)
 
-	vasyaPerms := repo.GetUser("vasya")
-
-	res := vasyaPerms.Has(auth.Read)
-	if res {
-		fmt.Printf("Право %s есть у пользователя %s\n", "Read", "vasya")
-	} else {
-		fmt.Println("Нет права")
+	for _, v := range users {
+		fmt.Println(v)
 	}
 
-	permList := auth.DecodePermissions(vasyaPerms)
-	fmt.Printf("Все права пользователя %s: %v", "vasya", permList)
+	////////
+
+	// repo := repository.UserRepository{
+	// 	Users: map[string]auth.Permission{
+	// 		"keker666": 3,
+	// 		"vasya":    7,
+	// 		"poleno":   7,
+	// 	},
+	// }
+
+	// /*roles*/
+	// kekerPerms := repo.GetUser("keker666")
+	// fmt.Println(kekerPerms)
+	// //добавили рута
+	// kekerPerms = kekerPerms.Add(auth.Root)
+	// fmt.Println(kekerPerms)
+
+	// //
+	// repo.UpdateUser("keker666", kekerPerms)
+
+	// //убрали рута
+	// kekerPerms = kekerPerms.Remove(auth.Root)
+
+	// //////
+
+	// vasyaPerms := repo.GetUser("vasya")
+
+	// res := vasyaPerms.Has(auth.Read)
+	// if res {
+	// 	fmt.Printf("Право %s есть у пользователя %s\n", "Read", "vasya")
+	// } else {
+	// 	fmt.Println("Нет права")
+	// }
+
+	// permList := auth.DecodePermissions(vasyaPerms)
+	// fmt.Printf("Все права пользователя %s: %v", "vasya", permList)
 
 }
--- internal/repository/memory_users_repository.go
+++ internal/repository/memory_users_repository.go
@@ -1,29 +1,114 @@
 package repository
 
 import (
+	"database/sql"
+	"fmt"
 	"permission-service/internal/auth"
 
+	"github.com/google/uuid"
 	_ "github.com/google/uuid"
 )
 
-// отдельный тип    не объект!!
-type UserRepository struct {
-	Users map[string]auth.Permission
-	// id uuid
+// // отдельный тип    не объект!!
+// type UserRepository struct {
+// 	Users map[string]auth.Permission
+// 	// id uuid
+// }
+
+type User struct {
+	id       uuid.UUID
+	username string
+	password string
+	role     auth.Permission
 }
 
-func (u *UserRepository) GetUser(name string) auth.Permission {
-	return u.Users[name]
+func (u *User) CreateUser(
+	dbConn *sql.DB,
+	id uuid.UUID,
+	username string,
+	password string,
+	role auth.Permission) (sql.Result, error) {
+
+	query := `
+	INSERT INTO users (id, username, password, role)
+	VALUES
+	($1, $2, $3, $4)
+	`
+
+	result, err := dbConn.Exec(query, id, username, password, role)
+
+	if err != nil {
+		fmt.Println("Error to create user: ", err)
+
+	}
+
+	return result, err
 }
 
-func (u *UserRepository) DeleteUser(name string) {
-	delete(u.Users, name)
+func (u *User) GetUser(
+	dbConn *sql.DB,
+	id uuid.UUID) (*User, error) {
+
+	query := `
+	SELECT id,
+	username,
+	password,
+	role
+	FROM users
+	WHERE id = $1
+	`
+	var user User
+
+	err := dbConn.QueryRow(query, id).Scan(
+		&user.id,
+		&user.username,
+		&user.password,
+		&user.role)
+
+	if err != nil {
+		fmt.Println("Error to get one user: ", err)
+		return nil, err
+	}
+	return &user, err
+
 }
 
-func (u *UserRepository) UpdateUser(name string, permission auth.Permission) {
-	u.Users[name] = permission
+func (u *User) GetAllUsers(dbConn *sql.DB) ([]*User, error) {
+	query := `SELECT * FROM users`
+	users := []*User{}
+
+	rows, err := dbConn.Query(query)
+
+	if err != nil {
+		fmt.Println("Problem to get all users: ", err)
+		return nil, err
+	}
+
+	defer rows.Close()
+
+	for rows.Next() {
+		user := User{}
+
+		err := rows.Scan(
+			&user.id,
+			&user.username,
+			&user.password,
+			&user.role,
+		)
+
+		if err != nil {
+			fmt.Println("Problem to get all users: ", err)
+			return nil, err
+		}
+
+		users = append(users, &user)
+	}
+	return users, err
 }
 
-func (u *UserRepository) CreateUser(name string, permission auth.Permission) {
-	u.Users[name] = permission
+func (u *User) DeleteUser(name string) {
+}
+
+func (u *User) UpdateUser(name string, permission auth.Permission) {
+
 }
